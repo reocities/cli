1be3efe65cb517ee2c60ba5c9e04cbe704ad4570
reocities <danyalassialt@gmail.com>
2025-07-02 11:36:33 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="reocities-cli",
+    version="1.0.0",
+    author="Reocities",
+    author_email="support@reocities.xyz",
+    description="Command line interface for managing your Reocities site",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/reocities/cli",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "reocities=reocities.cli:main",
+        ],
+    },
+)
